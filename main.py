69bdd8e6c9351ac3f2dd6099321a79f831f66291
Zeynep <146364601+zeysnepk@users.noreply.github.com>
2025-02-27 14:03:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src.scraper.trendyol import Trendyol
 from src.utils.dosya import Dosya
 import src.utils.mail as mail
-import asyncio
 import socket
+import time
 
 # İstenilen ürünlerin url lerinin bulunduğu dosya
 urls_dosya_adi = "config/urls.txt"
@@ -37,24 +37,29 @@ dosya_yaz(urls_dosya_adi, urls_listesi)
 urun_bilgileri = silinmis_linkleri_cikar(urls_listesi, urun_bilgileri)
 sure = 60 # Kontrol için saniye aralığı
 
-async def urunler_kontrol(url, trendyol):
+def urunler_kontrol(url, trendyol):
     try:
-        await trendyol.urun_sayfa_ac(url)
-        ad, fiyat = await trendyol.urun_bilgileri_gonder()
-        print(ad, fiyat)
-        await trendyol.urun_sayfa_kapa()
+        soup = trendyol.urun_sayfa_ac(url)
+        ad, fiyat, son_gun_fiyat = trendyol.urun_bilgileri_gonder(soup)
+        print(ad, fiyat, son_gun_fiyat)
         urun = next((urun for urun in urun_bilgileri if urun["url"] == url), None)
         if urun:
-            if fiyat < urun["fiyat"]:
-                mail.mail_gonder(ad, fiyat, urun["fiyat"], url)
-                print(f"[Fiyat Güncellendi] {ad} | Yeni: {fiyat} | Eski: {urun['fiyat']}\n")
-                urun["fiyat"] = fiyat # Yeni fiyatı güncelle
+            if fiyat < urun["simdiki_fiyat"]:
+                mail.mail_gonder(ad, fiyat, urun["simdiki_fiyat"], url, son_gun_fiyat)
+                print(f"[Fiyat Güncellendi] {ad} | Yeni: {fiyat} | Eski: {urun['simdiki_fiyat']}\n")
+                urun["onceki_fiyat"] = urun["simdiki_fiyat"]
+                urun["simdiki_fiyat"] = fiyat # Yeni fiyatı güncelle
+                return True
+            elif fiyat > urun["simdiki_fiyat"]:
+                print(f"[Fiyat Arttı] {ad} | Yeni: {fiyat} | Eski: {urun['simdiki_fiyat']}\n")
+                urun["onceki_fiyat"] = urun["simdiki_fiyat"]
+                urun["simdiki_fiyat"] = fiyat # Yeni fiyatı güncelle
                 return True
             else:
                 print(f"[Fiyat Aynı] {ad} | Fiyat: {fiyat}\n")
         else:
-            print(f"[Urun Bulunamadı] {url}\n")
-            data = {"urun_ad": ad, "fiyat": fiyat, "url": url}  
+            print(f"[Ürün bilgileri JSON dosyasına eklendi] {url}\n")
+            data = {"urun_ad": ad, "simdiki_fiyat": fiyat, "onceki_fiyat" : 0, "url": url}  
             urun_bilgileri.append(data)  
             return True
         return False
@@ -64,7 +69,7 @@ async def urunler_kontrol(url, trendyol):
 
 
 # İnternet kontrolü yapar
-async def internet_kontrol():
+def internet_kontrol():
     try:
         socket.create_connection(("8.8.8.8", 53), timeout=5)
         return True
@@ -73,28 +78,26 @@ async def internet_kontrol():
         return False
 
 # Ana fonksiyon
-async def main():
+def main():
     try:
         while True:
             # Chrome aç
             trendyol = Trendyol()
-            await trendyol.browser_ac()
-            if not await internet_kontrol():
+            if not internet_kontrol():
                 print("Bağlantı yok, tekrar deneyecek...")
-                await asyncio.sleep(60)
+                time.sleep(60)
                 continue
             for url in urls_listesi:
-                if await urunler_kontrol(url, trendyol):
+                if urunler_kontrol(url, trendyol):
                     dosya_yaz(bilgiler_dosya_adi, urun_bilgileri)
-            await asyncio.sleep(sure)          
+            print(f"{sure} saniye sonra tekrar kontrol edilecek...")
+            time.sleep(sure)          
     except Exception as e:
         print(f"Hata: {str(e)}")
         print("Tekrar başlatılıyor...")
-        await asyncio.sleep(10) 
-        await main()
-    finally: 
-        await trendyol.browser_kapa()
+        time.sleep(10) 
+        main()
 
-asyncio.run(main())
+main()
 
 
